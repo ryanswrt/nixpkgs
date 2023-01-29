2a91e9ccbb6ca8842f2955ea597b02edd49de984
Ryan Swart <ryan@talosystems.com>
2023-01-29 17:17:15 +0800

    init: dvc-gs
    Needed to fix dvc google-cloud support, also bumps fsspec and gcsfs

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2874,6 +2874,8 @@ self: super: with self; {
 
   dvc-data = callPackage ../development/python-modules/dvc-data {  };
 
+  dvc-gs = callPackage ../development/python-modules/dvc-gs {  };
+
   dvc-http = callPackage ../development/python-modules/dvc-http {  };
 
   dvc-objects = callPackage ../development/python-modules/dvc-objects {  };
